59da411a9d423babe7167a1cab928e2787fde438
ki-chi <k.brilliant@gmail.com>
2020-03-28 10:59:55 +0900

    Add definition file of the package

---
+++ julia-formatter-pkg.el
@@ -0,0 +1,4 @@
+(define-package "julia-formatter" "0.1.0"
+                "Emacs package for formatting Julia code using JuliaFormatter.jl"
+                '((emacs "24.4")
+                  (json "1.3")))
